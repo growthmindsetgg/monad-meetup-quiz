cf828a08276519d071a34da9cbd27db6833c68e1
Durgesh <manufan728@gmail.com>
2026-03-12 13:02:09 +0530

    initial quiz app setup

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+export default function App() {
+  return (
+    <div style={{
+      background: "#0f172a",
+      height: "100vh",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      color: "white",
+      fontSize: "40px",
+      fontWeight: "bold"
+    }}>
+      Monad Meetup Quiz
+    </div>
+  )
+}
